e7dd644671fa41ac8ee6e6988dee145cac34bb05
Nabil <nabil.noble01@gmail.com>
2022-12-19 21:20:28 +0000

    add mandory files

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const myArray = [];
+  for (const value of array) {
+    myArray.push(`${appendString}${value}`);
+  }
+  return myArray;
+}
