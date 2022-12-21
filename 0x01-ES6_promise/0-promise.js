105aaa9babba528d81b03c1d042f428781b7268c
Nabil <nabil.noble01@gmail.com>
2022-12-21 15:09:49 +0000

    add 0#

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        resolve('true');
-        reject('false');
-    });
+  return new Promise((resolve) => {
+    resolve('true');
+  });
 }
