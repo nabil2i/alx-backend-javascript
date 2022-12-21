b03857c3b2ee893d92bb8df5508ce8c26dbbd45a
Nabil <nabil.noble01@gmail.com>
2022-12-21 18:45:03 +0000

    edit 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-    const employees = [];
-    for (const r of reportWithIterator) {
-      employees.push(r);
-    }
-  
-    return employees.join(' | ');
+  const employees = [];
+  for (const r of reportWithIterator) {
+    employees.push(r);
+  }
+
+  return employees.join(' | ');
 }
