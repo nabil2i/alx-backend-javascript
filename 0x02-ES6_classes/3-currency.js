3911ded91dcae765ca0d4536fd5c22b148875214
Nabil <nabil.noble01@gmail.com>
2022-12-22 12:44:09 +0000

    add 3#

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,50 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get code() {
+    return this.getName();
+  }
+
+  getCode() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this.getName();
+  }
+
+  getName() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
