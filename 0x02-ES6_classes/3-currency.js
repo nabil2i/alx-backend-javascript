1c9051a64f850326a43d3354fcf3a10c667175fb
Nabil <nabil.noble01@gmail.com>
2022-12-22 12:48:14 +0000

    edit 3#

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -20,9 +20,9 @@ export default class Currency {
     return this._code;
   }
 
-  set code(code) {
-    if (typeof code === 'string') {
-      this._code = code;
+  set code(val) {
+    if (typeof val === 'string') {
+      this._code = val;
     } else {
       throw new TypeError('Code must be a string');
     }
@@ -36,9 +36,9 @@ export default class Currency {
     return this._name;
   }
 
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
+  set name(val) {
+    if (typeof val === 'string') {
+      this._name = val;
     } else {
       throw new TypeError('Name must be a string');
     }
