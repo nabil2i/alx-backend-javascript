52673e3b82a186bdc6f8d68159b57b91333c4d30
Nabil <nabil.noble01@gmail.com>
2022-12-22 12:05:48 +0000

    add 2# a,d edit 1#

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const myArray = [];
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(10);
-  myArray.push(room1);
-  myArray.push(room2);
-  myArray.push(room3);
-  return myArray;
+  const rooms = [];
+  const numbers = [19, 20, 34];
+  let i = 0;
+  while (i < numbers.length) {
+    rooms.push(new ClassRoom(numbers[i]));
+    i += 1;
+  }
+  return rooms;
 }
