873b586695c1cfc96fc28248ebe805ec2c44301b
Nabil <nabil.noble01@gmail.com>
2023-01-03 19:10:25 +0000

    edit 1#

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,8 +2,6 @@ export default function getListStudentIds(arrayObjects) {
   if (!arrayObjects || typeof arrayObjects !== 'object') {
     return [];
   }
-  else {
-    const map1 = arrayObjects.map((obj) => obj.id);
-    return map1;
-  }
+  const map1 = arrayObjects.map((obj) => obj.id);
+  return map1;
 }
